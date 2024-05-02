90e5251f852a975f58ba902f98fbb4fb1737331b
sorokin181 <168706776+sorokin181@users.noreply.github.com>
2024-05-02 22:30:27 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Кликер_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::кликер_3.Properties.Resources.дерево2;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(543, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(249, 275);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(-5, 503);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 100);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "улучшить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = global::кликер_3.Properties.Resources._91b5610d_no_bg_preview__carve_photos_;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(7, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(206, 100);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "      Очки: 0";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(-5, 345);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(206, 100);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Стоимость\r\nулучшения: 10";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(16, 168);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(206, 100);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Количество\r\nулучшений: 0";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(520, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "+1";
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.BackgroundImage = global::кликер_3.Properties.Resources._9dafa855_no_bg_preview__carve_photos___1_;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(923, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(65, 57);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button7.BackColor = System.Drawing.Color.Transparent;
+            this.button7.BackgroundImage = global::кликер_3.Properties.Resources._995605c8_no_bg_preview__carve_photos_;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(830, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(78, 57);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::кликер_3.Properties.Resources.старт1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1000, 590);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Кликер_2
+{
+    public partial class Form1 : Form
+    {
+
+        public int money = 0;
+        public int upgred = 10;
+        public int multi = 1;
+        public int count = 0;
+        public int plus = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        protected override void WndProc(ref Message message)
+        {
+            if (message.Msg == 0x201)
+            {
+                base.Capture = false;
+                message = Message.Create(base.Handle, 0xA1, new IntPtr(2), IntPtr.Zero);
+            }
+            base.WndProc(ref message);
+        }
+
+        private Cursor cur;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Cursor = new Cursor(@"C:\Users\Lenovo\Desktop\проэкты\Кликер 2\фотки\курсоры\axe.Cur");
+
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            label1.Visible = false;
+
+            
+            //this.BackgroundImage = Properties.Resources.старт1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            money += multi;
+            button3.Text = $"      Очки: {money} ";
+
+            //label4.Visible = true;
+
+            Random rnd = new Random();
+            int a = rnd.Next();
+
+            label1.Visible = true;
+            label1.Left = rnd.Next(button1.Width, 550) - button1.Width;
+            label1.Top = rnd.Next(button1.Height, 500) - button1.Height;
+
+            button1.Cursor = new Cursor(@"C:\Users\Lenovo\Desktop\проэкты\Кликер 2\фотки\курсоры\axe.Cur");
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (money >= upgred)
+            {
+                money -= upgred;
+                button3.Text = $"      Очки: {money}";
+                multi++;
+                upgred = upgred * 2;
+                button4.Text = $"Стоимость \n улутшеня: {upgred}";
+                count++;
+                button5.Text = $"количество \n улучшений: {count}";
+                plus++;
+                label1.Text = $"+{Convert.ToString(plus)}";
+                
+            }
+
+            if (count >= 2)
+            {
+                BackgroundImage = Image.FromFile("C:\\Users\\Lenovo\\Desktop\\проэкты\\Кликер 2\\Кликер 2\\Resources\\горы1.png");
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            button1.BackgroundImage = Image.FromFile("C:\\Users\\Lenovo\\Desktop\\проэкты\\Кликер 2\\Кликер 2\\Resources\\дерево2.1.png");
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            button1.BackgroundImage = Image.FromFile("C:\\Users\\Lenovo\\Desktop\\проэкты\\Кликер 2\\Кликер 2\\Resources\\дерево2.png");
+        }
+
+        private void button1_CursorChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.Cursor = new Cursor(@"C:\Users\Lenovo\Desktop\проэкты\Кликер 2\фотки\курсоры\axe.Cur");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private bool x = true;
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+                if (x == true) 
+                {
+                this.TopMost = true;
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Normal;
+                x = false;
+                }
+                else 
+                {
+                this.TopMost = true;
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Maximized;
+                x = true;
+                }
+  
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        
+        }
+    }
+}
